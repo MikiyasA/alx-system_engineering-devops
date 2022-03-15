203ef067955f7ba6ebfc234ef99cc302ea0d8bf0
MikiyasA <mikiyasad86@gmail.com>
2022-03-15 15:44:58 -0700

    To fix some error

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # puppet script to configure nginx web server 
 
-exec { 'apt_install':
-  provider => shell;
-  command  => apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default; sudo service nginx restart
+exec { 'install':
+  provider => shell,
+  command  => apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default; sudo service nginx restart,
 }
